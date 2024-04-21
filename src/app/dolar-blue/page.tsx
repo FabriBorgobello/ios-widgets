caa022c963d9bd3d499b66026e86d5d1e8235c44
Fabricio Borgobello <fborgobello@gmail.com>
2024-04-21 22:18:18 +0200

    Improve dark mode

--- src/app/dolar-blue/page.tsx
+++ src/app/dolar-blue/page.tsx
@@ -19,26 +19,26 @@ export default async function ArsUsd() {
   const { buy, sell, date } = await fetchUSDValue();
   return (
     <div className="flex h-screen w-screen items-center justify-center bg-zinc-800">
-      <div className="flex aspect-square w-80 flex-col items-center justify-center gap-3 bg-zinc-950 p-8 text-zinc-50">
+      <div className="flex aspect-square w-80 flex-col items-center justify-center gap-3 bg-zinc-50 p-8 text-zinc-950 dark:bg-zinc-950 dark:text-zinc-50">
         <div className="flex items-center gap-2">
           <span className="text-3xl">🇦🇷</span>
           <h1 className="text-xl font-semibold">Dólar Blue</h1>
           <span className="text-3xl">💵</span>
         </div>
-        <hr className="w-full border border-zinc-400 opacity-30" />
+        <hr className="w-full border border-zinc-400 opacity-30 dark:border-zinc-400" />
         <p className="flex flex-col items-center">
-          <span className="text-zinc-400">Compra</span>
+          <span className="text-zinc-800 dark:text-zinc-400">Compra</span>
           <span className="text-2xl font-semibold">
             {buy ? formatToARS(buy) : "-"}
           </span>
         </p>
         <p className="flex flex-col items-center">
-          <span className="text-zinc-400">Venta</span>
+          <span className="text-zinc-800 dark:text-zinc-400">Venta</span>
           <span className="text-2xl font-semibold">
             {sell ? formatToARS(sell) : "-"}
           </span>
         </p>
-        <time className="mt-3 text-sm font-light text-zinc-400">
+        <time className="mt-3 text-sm font-light text-zinc-800 dark:text-zinc-400">
           Actualizado: {date}
         </time>
       </div>
